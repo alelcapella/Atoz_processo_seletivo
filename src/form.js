9f97a99f92848c9c08c9d3d3ddb02cf2b4882c2f
alelcapella <71743924+alelcapella@users.noreply.github.com>
2021-04-21 16:28:52 -0300

    Add files via upload

--- src/form.js
+++ src/form.js
@@ -1,6 +1,6 @@
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import {CContainer, CRow, CCol, CForm, CButton, CLabel, CInput} from "@coreui/react";
+import { CContainer, CRow, CCol, CForm, CButton, CLabel, CInput } from "@coreui/react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 
@@ -8,15 +8,15 @@ const Form = () => (
 
 
     <Formik
-        initialValues={{ name: "",  email: "", phone: "", password: "", confirmPassword: ""}}
+        initialValues={{ name: "", email: "", phone: "", password: "", confirmPassword: "" }}
 
         onSubmit={(values, { setSubmitting }) => {
             setTimeout(() => {
-              console.log(JSON.stringify(values, null, 2));
-              alert("Dados enviados! Obrigado!");
-              setSubmitting(false);
+                console.log(JSON.stringify(values, null, 2));
+                alert("Dados enviados! Obrigado!");
+                setSubmitting(false);
             }, 400);
-          }}
+        }}
 
         validationSchema={Yup.object().shape({
             name: Yup.string().min(6, 'Pelo menos 6 caracteres.').required('Preencher nome.'),
@@ -30,40 +30,40 @@ const Form = () => (
         {props => {
             const {
                 values,
-         errors,
-         touched,
-         handleChange,
-         handleBlur,
-         handleSubmit,
-         isSubmitting
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting
             } = props;
 
             return (
-                
+
                 <CContainer>
-                <CForm onSubmit={handleSubmit}>
-                    <CRow className="form-left">
-                     <CCol md="6" sm="12">
+                    <CForm onSubmit={handleSubmit}>
+                        <CRow className="form-left">
+                            <CCol md="6" sm="12">
                                 <CLabel htmlFor="name">Nome</CLabel>
                                 <CInput
                                     name="name"
                                     type="text"
                                     onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.name}
+                                    onBlur={handleBlur}
+                                    value={values.name}
                                     className={
                                         errors.name && touched.name
                                             ? "text-input error"
                                             : "text-input"
                                     }
                                 />
-                                
+
 
                                 {errors.name && touched.name && (
                                     <div className="input-feedback">{errors.name}</div>
                                 )}
-                               
-                               <CLabel htmlFor="email">E-mail</CLabel>
+
+                                <CLabel htmlFor="email">E-mail</CLabel>
                                 <CInput
                                     name="email"
                                     type="text"
@@ -76,7 +76,7 @@ const Form = () => (
                                             : "text-input"
                                     }
                                 />
-                                
+
 
                                 {errors.email && touched.email && (
                                     <div className="input-feedback">{errors.email}</div>
@@ -94,17 +94,17 @@ const Form = () => (
                                         errors.phone && touched.phone
                                             ? "text-input error"
                                             : "text-input"
-                                    } 
+                                    }
                                 />
-                                
-                                
+
+
                                 {errors.phone && touched.phone && (
                                     <div className="input-feedback">{errors.phone}</div>
                                 )}
 
-</CCol>
+                            </CCol>
 
-                                <CCol md="6" sm="12">
+                            <CCol md="6" sm="12">
                                 <CLabel htmlFor="password">Senha</CLabel>
                                 <CInput
                                     name="password"
@@ -119,7 +119,7 @@ const Form = () => (
                                             : "text-input"
                                     }
                                 />
-                                
+
                                 {errors.password && touched.password && (
                                     <div className="input-feedback">{errors.password}</div>
                                 )}
@@ -138,20 +138,20 @@ const Form = () => (
                                             : "text-input"
                                     }
                                 />
-                                
+
 
                                 {errors.confirmPassword && touched.confirmPassword && (
                                     <div className="input-feedback">{errors.confirmPassword}</div>
                                 )}
-                                    
-                            <CButton className="salvar" type="submit" disabled={isSubmitting} onSubmit={handleSubmit}>
-                                Enviar
+
+                                <CButton className="salvar" type="submit" disabled={isSubmitting} onSubmit={handleSubmit}>
+                                    Enviar
                                 </CButton>
-                                 </CCol>
-                                 </CRow>
-                </CForm>
+                            </CCol>
+                        </CRow>
+                    </CForm>
                 </CContainer>
-                
+
             );
         }}
 
@@ -159,9 +159,9 @@ const Form = () => (
 
 
     </Formik>
-   
-      
-    
+
+
+
 
 )
 
